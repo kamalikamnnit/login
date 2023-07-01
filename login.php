a4c5c505a8638cdeaeedc0a6ff20cd5e10f3eca4
kamalikamnnit <127094035+kamalikamnnit@users.noreply.github.com>
2023-07-01 13:32:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,38 @@
 <?php
-mysqli_connect("localhost","root","","newfile");
-if(mysqli_connect_error())
-{
-    echo "Cannot Connect";
+session_start();
+
+$con = mysqli_connect('localhost','root');
+
+if($con){
+    echo "connection successful";
 }
+
 else{
-    echo "Connected";
+    echo "no connection";
 }
+
+mysqli_select_db($con,'sessionpractical');
+
+$username = $_POST['username'];
+$password =$_POST['password'];
+
+$q = " select * from signin where username = '$username' && password = '$password'";
+
+$result =mysqli_query($con,$q);
+$num = mysqli_num_rows($result);
+if($num == 1)
+{
+$_SESSION['username'] = $username;
+    
+    header('location:home.php');
+}  
+
+else{
+   echo "Inalid username or password";
+   header('location:info.php');
+    
+}
+
+
+
 ?>
